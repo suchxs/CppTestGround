b99ffcd737d2d0a78167ee85a651336007fed679
watsonjph <corsegajoseph@gmail.com>
2025-07-02 18:32:30 +0800

    New Proj

---
+++ TestTusTyRone/Main.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <climits>
+
+
+int main()
+{
+    int x{ 12 };
+    std::cout << x << '\n'; // decimal (by default)
+    std::cout << std::hex << x << '\n'; // hexadecimal
+    std::cout << x << '\n'; // now hexadecimal
+    std::cout << std::oct << x << '\n'; // octal
+    std::cout << std::dec << x << '\n'; // return to decimal
+    std::cout << x << '\n'; // decimal
+
+    return 0;
+}
