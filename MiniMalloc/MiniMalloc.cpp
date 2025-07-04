b02191ec7d7a90ca40e37793ac2cde84f5bccf10
watsonjph <corsegajoseph@gmail.com>
2025-07-04 18:42:38 +0800

    MiniAlloc Update

--- MiniMalloc/MiniMalloc.cpp
+++ MiniMalloc/MiniMalloc.cpp
@@ -2,14 +2,25 @@
 
 // Cpp PlayGround Mini Malloc
 
+// Goal - void* minimalloc(size_t size)
+
 using namespace std;
 
 
-
+struct Block {
+	size_t size; // Size of Block (in bytes)
+	Block* next; // Pointer to Next Byte
+};
 
 int main() {
 
+	char* memoryPool = nullptr;
+	size_t poolSize = 0;
 	cout << "Will put some stuff here As I learn about shit";
 
 	return 0;
 }
+
+
+/* To do:
+*/
